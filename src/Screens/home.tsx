7e993824432964ed6ae683e63ca3d82e8a3cb6ee
iftakhar <iftakhar167@gmail.com>
2022-12-10 15:21:01 +0100

    Test

--- src/Screens/home.tsx
+++ src/Screens/home.tsx
@@ -1,10 +1,20 @@
+import { alignProperty } from '@mui/material/styles/cssUtils'
+import { margin } from '@mui/system'
 import TableDrawer from '../Components/tableDrawer/TableDrawer'
 
 const Home = () => {
+
+    const test = () => {
+        alert("test")
+    }
     return (
         <div>
             <h1>Home</h1>
-            <TableDrawer />
+
+            {/* <TableDrawer /> */}
+
+            {/* <button style={{marginTop:"199%", marginLeft:"42%", padding:"30px"}} onClick={test}>Test</button> */}
+
         </div>
     )
 }
